9b0a9e33bdd0d9c43fa97c7b96601bb69e5b3d2a
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-10-13 05:33:53 -0400

    initial fancyness of header component with looping images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,10 +9,16 @@ import { HeaderService } from './header.service';
 })
 export class HeaderComponent {
   title: string = 'FUNDABITAT';
+  images: string[];
 
   constructor(private headerSvc: HeaderService ) {
     this.headerSvc.whenTitleChanges.subscribe((title: string) => {
       this.title = title;
     })
+
+    // TODO ADD Missing last (9th) image since its jpg
+    let simples: string[] = Array(8).fill(0).map((e, i) => `/assets/images/header/${i+1}.png`);
+    this.images = simples.concat(simples);
   }
+
 }
