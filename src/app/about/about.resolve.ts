1b9f0a6440671a507c0b0eabe188c0dcfd276cc7
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-11-28 03:36:55 -0400

    include typing information for correct aot compilation

--- src/app/about/about.resolve.ts
+++ src/app/about/about.resolve.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve } from '@angular/router';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
+import { Observable } from "rxjs";
+
 @Injectable()
 export class AboutResolve implements Resolve<FirebaseObjectObservable<any>> {
 
   constructor(private fire: AngularFire) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FirebaseObjectObservable<any>> {
     let obs = this.fire.database.object('/about');
     return obs.first().map(() => obs);
   }
