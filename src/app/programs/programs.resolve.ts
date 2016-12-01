706066b12ada5db6e6d5e457dda827bc18ca4aeb
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-12-01 13:10:26 -0400

    change programs template to receive sections of programs (and add basic db data of it)

--- src/app/programs/programs.resolve.ts
+++ src/app/programs/programs.resolve.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve } from '@angular/router';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
+import { mapValues, values } from 'lodash';
 
 @Injectable()
 export class ProgramsResolve implements Resolve<FirebaseListObservable<any>> {
@@ -10,7 +11,14 @@ export class ProgramsResolve implements Resolve<FirebaseListObservable<any>> {
   constructor(private fire: AngularFire) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FirebaseListObservable<any>> {
-    let obs = this.fire.database.list('/programs');
+    let obs = this.fire.database.list('/programs').map(schemaTransform);
     return obs.first().map(() => obs);
   }
+}
+
+function schemaTransform(sections: any) {
+  return sections.map((sect) => mapValues(sect, (value, key) => {
+    if (key === 'programs') return values(value);
+    return value;
+  }));
 }
