e28d668c4c8e54261baa37b08690d4a4a741fbb5
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-12-04 18:07:39 -0400

    change selector prefix to fundabitat shortcut

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth',
+  selector: 'fd-auth',
   template: '<router-outlet></router-outlet>'
 })
 export class AuthComponent {
