fa19c4fb4d6dcd18ffa57f79f20ea3e561f7a958
marylu <marylo281292@gmail.com>
2017-03-04 17:43:08 -0400

    Fixed header details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,9 +14,8 @@ export class HeaderComponent {
   constructor(private headerSvc: HeaderService ) {
     this.headerSvc.whenTitleChanges.subscribe((title: string) => {
       this.title = title;
-    })
+    });
 
-    // TODO ADD Missing (7th and 9th) image since its jpg
     let simples: string[] = Array(8).fill(0).map((e, i) => `/assets/images/header/${i+1}.jpg`);
     this.images = simples.concat(simples);
   }
