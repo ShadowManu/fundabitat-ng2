c2fedbfea60a26263b19af42d61a8e66af3e26a3
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-11-11 04:54:06 -0400

    update admin programs

--- src/app/admin/programs/admin-programs.component.ts
+++ src/app/admin/programs/admin-programs.component.ts
@@ -3,10 +3,12 @@ import { NgForm } from '@angular/forms';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AngularFirestore } from 'angularfire2/firestore';
-// import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 import { Observable } from 'rxjs/Observable';
+import { combineLatest } from 'rxjs/observable/combineLatest';
 import { assign, values } from 'lodash';
+
+import { mixDocuments, normalizeDocArray, Program } from 'app/core';
 
 @Component({
   selector: 'fd-admin-programs',
@@ -14,28 +16,24 @@ import { assign, values } from 'lodash';
   styleUrls: ['./admin-programs.component.scss']
 })
 export class AdminProgramsComponent {
-  sections$ = this.firestore.collection('program-sections').snapshotChanges()
-  .do(a => console.log(a));
+  sections$ = this.firestore.collection('program-sections').snapshotChanges().map(normalizeDocArray);
+
+  sectionsWithPrograms$ = combineLatest(
+    this.firestore.collection('program-sections').snapshotChanges(),
+    this.firestore.collection('programs').snapshotChanges(),
+    mixDocuments
+  );
 
   constructor(
     private firestore: AngularFirestore,
     private snackBar: MatSnackBar
   ) { }
 
-  onCreateSection(form: NgForm) {
-    this.firestore.collection('program-sections').add(form.value)
-    .then(() => this.snackBar.open('Sección creada exitosamente', 'Cerrar', { duration: 4000 }))
-    .catch(() => this.snackBar.open('Error al crear sección', 'Cerrar', { duration: 4000 }));
-  }
-
   onCreateProgram(form: NgForm) {
-    console.log(form.value);
+    let program: Program = form.value;
 
-    // this.fire.database.list(`/programs/${section}/programs`)
-    // .push({ title, description, location })
-    // .then(() => this.snackBar.open('Programa creada exitosamente', undefined, { duration: 2000 }))
-    // .catch(() => this.snackBar.open('El programa no pudo crearse', undefined, { duration: 2000 }));
-
-    // this.newProgram.reset();
+    this.firestore.collection('programs').add(program)
+    .then(() => this.snackBar.open('Programa creado exitosamente', 'Cerrar', { duration: 4000 }))
+    .catch(() => this.snackBar.open('Error al crear el programa', 'Cerrar', { duration: 4000 }));
   }
 }
