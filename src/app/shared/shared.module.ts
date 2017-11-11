69a6ac65a40ba11b5e8fdfb92ad6418422817d33
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-11-11 03:41:23 -0400

    add firesrc directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { DividerComponent } from './divider/divider.component';
+import { FireSrcDirective } from './fire-src/fire-src.directive';
 
 @NgModule({
   declarations: [
-    DividerComponent
+    DividerComponent,
+    FireSrcDirective
   ],
   imports: [
     BrowserModule,
   ],
   exports: [
-    DividerComponent
+    DividerComponent,
+    FireSrcDirective
   ]
 })
 export class SharedModule { }
