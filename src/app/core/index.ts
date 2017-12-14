c69b739b84b86b8584e88ae24760e40fef2d034d
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-12-14 05:44:20 -0400

    initial multilanguage support

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -3,6 +3,7 @@ export * from './helpers';
 export * from './types';
 
 export * from './auth.service';
+export * from './language.service';
 export * from './programs.service';
 
 export * from './auth.guard';
---
+++ src/app/core/language.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+export type Language = 'es' | 'en';
+
+export type LanguageData<T> = Map<Language, T>;
+
+const LANGUAGE_STORAGE_KEY = 'language';
+
+const LANGUAGE_PREFIXES = new Map<Language, string>([
+  ['es', ''],
+  ['en', '-en']
+]);
+
+@Injectable()
+export class LanguageService {
+  language = this.readFromStorage();
+
+  get prefix() { return LANGUAGE_PREFIXES.get(this.language); }
+
+  setLanguage(lang: Language) {
+    this.language = lang;
+    this.setStorage(lang);
+  }
+
+  select<T>(data: LanguageData<T>): T { return data.get(this.language); }
+
+  setStorage(lang: Language) { localStorage.setItem(LANGUAGE_STORAGE_KEY, lang); }
+
+  readFromStorage(): Language { return (localStorage.getItem(LANGUAGE_STORAGE_KEY) as Language) || 'es'; }
+}
