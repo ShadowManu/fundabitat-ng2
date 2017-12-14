4bd4f60990b225eb0264b731818c423fa6a9fc43
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-12-13 22:30:04 -0400

    add auth service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material'
 
+import { AuthService } from './auth.service';
 import { ProgramsService } from './programs.service';
 
 @NgModule({
+  imports: [
+    MatSnackBarModule
+  ],
   providers: [
+    AuthService,
     ProgramsService
   ]
 })
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,6 +2,7 @@ export * from './colors';
 export * from './helpers';
 export * from './types';
 
+export * from './auth.service';
 export * from './programs.service';
 
 export * from './core.module';
