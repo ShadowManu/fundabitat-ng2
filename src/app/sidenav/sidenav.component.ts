cbfad1e746719c9fea9b9ecb60f0b0afd0ec8640
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-12-14 06:45:00 -0400

    add multilanguage support for sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core';
 
-import { RIPPLE_COLOR } from 'app/core';
+import { RIPPLE_COLOR, LanguageService, Language, LanguageData } from 'app/core';
 
-const NAVIGATION_ITEMS = [
-  {
-    name: 'Quienes Somos', link: "/nosotros",
-    childs: [
-      { name: 'Nuestro equipo', link: "/nosotros/equipo" },
-      { name: 'Relaciones', link: "/nosotros/relaciones" },
-    ]
-  },
-  { name: 'Áreas Temáticas', link: "/areas" },
-  { name: 'Programas', link: "/programas" },
-  { name: 'Publicaciones', link: "/publicaciones" }
-];
+type NavigationData = {
+  name: string;
+  link: string;
+  childs?: NavigationData;
+}[];
+
+const NAVIGATION_DATA: LanguageData<NavigationData> = new Map<Language, NavigationData>([
+  ['es', [
+    {
+      name: 'Quienes Somos', link: '/nosotros',
+      childs: [
+        { name: 'Nuestro equipo', link: '/nosotros/equipo' },
+        { name: 'Relaciones', link: '/nosotros/relaciones' }
+      ]
+    },
+    { name: 'Áreas Temáticas', link: '/areas' },
+    { name: 'Programas', link: '/programas' },
+    { name: 'Publicaciones', link: '/publicaciones' }
+  ]],
+  ['en', [
+    {
+      name: 'About us', link: '/nosotros',
+      childs: [
+        { name: 'Our team', link: '/nosotros/equipo' },
+        { name: 'Relations', link: '/nosotros/relaciones' }
+      ]
+    },
+    { name: 'Thematic Areas', link: '/areas' },
+    { name: 'Programs', link: '/programas' },
+    { name: 'Publications', link: '/publicaciones' }
+  ]]
+]);
 
 @Component({
   selector: 'fd-sidenav',
@@ -21,6 +41,12 @@ const NAVIGATION_ITEMS = [
   styleUrls: ['sidenav.component.scss']
 })
 export class SidenavComponent {
-  get items() { return NAVIGATION_ITEMS; }
+
+  constructor(private langSvc: LanguageService) { }
+
+  get items() { return this.data; }
+
+  get data(): NavigationData { return this.langSvc.select(NAVIGATION_DATA); }
+
   get rippleColor() { return RIPPLE_COLOR; }
 }
